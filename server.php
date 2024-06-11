ffecdce9d1c741a289f98249c89afa02ce4211ab
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-11 16:01:31 +0200

    added style

--- server.php
+++ server.php
@@ -47,11 +47,13 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP HOTEL</title>
 </head>
 
 <body>
-    <h1>
+    <h1 class="text-center py-2">
         <?= $titlePage ?>
     </h1>
 
@@ -67,12 +69,13 @@ $hotels = [
         </ul> 
     -->
 
-    <ul>
+    <!-- STAMPO IN PAGINA TUTTI GLI HOTEL CON IL VOTO E SE HANNO PARCHEGGIO -->
+    <ul class="row m-0 p-0 justify-content-between">
         <?php
         foreach ($hotels as $hotel) {
-            echo "<li>" . "Il nome dell'hotel è:" . $hotel['name'] . "</li>";
-            echo "<li>" . " C'è parcheggio?" . $hotel['parking'] . "</li>";
-            echo "<li>" . " Il voto è: " . $hotel['vote'] . "</li>";
+            echo "<li class='bg-primary col-4 p-0 px-2 m-1'>" . "Il nome dell'hotel è:" . " " . $hotel['name'] . "</li>";
+            echo "<li class='bg-danger col-3 p-0 px-2 m-1'>" . " C'è parcheggio?" . " " . $hotel['parking'] . "</li>";
+            echo "<li class='bg-warning col-4 p-0 px-2 m-1'>" . " Il voto è: " . " " . $hotel['vote'] . "</li>";
         }
         ?>
     </ul>
