7565432bee324236520da90554cc4be09385e59b
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-11 16:05:32 +0200

    added ricerca.php

---
+++ ricerca.php
@@ -0,0 +1,18 @@
+<?php
+$ciao = "Ciao Giuseppe, funziona tutto";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RICERCA PHP</title>
+</head>
+
+<body>
+    <?= $ciao ?>
+</body>
+
+</html>
